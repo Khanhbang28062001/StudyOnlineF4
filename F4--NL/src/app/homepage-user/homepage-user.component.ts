fc8ed601e321d9b179eb8f7cd8d63a68ea440cb9
Khanhbang280601 <khanhbang280601@gmail.com>
2023-05-11 00:34:55 +0700

    fix router of user

--- F4--NL/src/app/homepage-user/homepage-user.component.ts
+++ F4--NL/src/app/homepage-user/homepage-user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserIDService } from '../Service/user-id.service';
+import { UserService } from '../Service/user.service';
+import { UserModule } from '../model/user.module';
 
 @Component({
   selector: 'app-homepage-user',
@@ -14,28 +17,46 @@ export class HomepageUserComponent {
   public TenEmail = "bang";
   public Email = "bang-Email";
 
-  constructor(private router:Router){}
-   
-  public Login(){
+
+  public Login() {
     this.router.navigate(['/login-form']);
   }
-  public signup(){
+  public signup() {
     this.router.navigate(['/signup-form']);
   }
-  openNavProfile(): void{
+  openNavProfile(): void {
     this.isDropDownOpen = !this.isDropDownOpen;
-    this.isDropDownOpenNotification= false;
-    this.isDropDownOpenBlog= false;
+    this.isDropDownOpenNotification = false;
+    this.isDropDownOpenBlog = false;
   }
-  openNavNotification(): void{
-    this.isDropDownOpenNotification= !this.isDropDownOpenNotification;
+  openNavNotification(): void {
+    this.isDropDownOpenNotification = !this.isDropDownOpenNotification;
     this.isDropDownOpen = false;
-    this.isDropDownOpenBlog= false;
+    this.isDropDownOpenBlog = false;
   }
-  openNavBlog(): void{
-    this.isDropDownOpenBlog= !this.isDropDownOpenBlog;
+  openNavBlog(): void {
+    this.isDropDownOpenBlog = !this.isDropDownOpenBlog;
     this.isDropDownOpen = false;
-    this.isDropDownOpenNotification= false;
+    this.isDropDownOpenNotification = false;
+  }
+  constructor(private userIdService: UserIDService,
+    private userService: UserService,
+    private router: Router
+
+  ) { }
+
+  userId: Number = 0;
+  User: UserModule = new UserModule(0, "", "", "", 0);
+  ngOnInit(): void {
+    // this.userId =this.userIdService.getCurrentUserId();
+    this.userIdService.currentUserId.subscribe(userId => this.userId = userId);
+    console.log(this.userId);
+    this.userService.getUserById(this.userId).subscribe((data) => {
+      this.User = data;
+      console.log("user gv data:" + data.username);
+    })
+    console.log("Lay du lieu user gv roi ne!!!!!!" + this.User);
+
   }
 
 }
